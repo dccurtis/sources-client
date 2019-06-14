57c2dd9bcc108ca6eb06ff8c53e8a803bdd900ac
Douglas Curtis <docurtis@redhat.com>
2019-06-14 13:12:11 -0400

    still working

--- sources_client/app.py
+++ sources_client/app.py
@@ -1,12 +1,28 @@
 import os
 import tornado.ioloop
 import tornado.web
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
 from tornado_sqlalchemy import make_session_factory
 from tornado.gen import coroutine
+from tornado.ioloop import IOLoop
 from tornado_sqlalchemy import SessionMixin, as_future
+from kafkahelpers import ReconnectingClient
+from aiokafka import AIOKafkaConsumer
 
 from models import create_all, User
 
+SOURCES_KAFKA_HOST = os.getenv('SOURCES_KAFKA_HOST', 'localhost')
+SOURCES_KAFKA_PORT = os.getenv('SOURCES_KAFKA_PORT', '29092')
+SOURCES_KAFKA_ADDRESS = f'{SOURCES_KAFKA_HOST}:{SOURCES_KAFKA_PORT}'
+SOURCES_TOPIC = os.getenv('SOURCES_KAFKA_TOPIC', 'platform.sources.event-stream')
+# kafka_consumer = AIOKafkaConsumer(SOURCES_TOPIC, loop=IOLoop.current().asyncio_loop,
+#                                  bootstrap_servers=SOURCES_KAFKA_ADDRESS, group_id='koku-group')
+# CONSUMER = ReconnectingClient(kafka_consumer, "consumer")
+LOOPS = {}
+
+thread_pool_executor = ThreadPoolExecutor(max_workers=10)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -41,9 +57,27 @@ class Application(tornado.web.Application):
 
         tornado.web.Application.__init__(self, handlers, session_factory=factory)
 
+async def handle_validation(client):
+    data = await client.getmany(timeout_ms=1000, max_records=1)
+    for tp, msgs in data.items():
+        if tp.topic == SOURCES_TOPIC:
+            logger.info("Processing %s messages from topic [%s]", len(msgs), tp.topic, extra={
+                "topic": tp.topic
+            })
+            # TODO: Figure out how to properly handle failures
+            # await asyncio.gather(*[handle_file(msg) for msg in msgs])
+            await asyncio.sleep(5)
+
+
 if __name__ == "__main__":
     app = Application()
     app.listen(8888)
     print('Listeneing on port 8888')
-    tornado.ioloop.IOLoop.current().start()
+    loop = IOLoop.current()
+    loop.set_default_executor(thread_pool_executor)
 
+    # LOOPS["consumer"] = IOLoop(make_current=False).instance()
+    # LOOPS["consumer"].add_callback(CONSUMER.get_callback(handle_validation))
+    # for loop in LOOPS.items():
+    #     loop.start()
+    # loop.start()
